adc1bbbb2d1559f33f40acad0414a44c370d5d25
Ederson Rodrigues <ederson@trilhante.com.br>
2023-07-07 17:59:39 -0300

    :wrench: basic config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepositionsModule } from './depositions/depositions.module';
 
 @Module({
-  imports: [],
+  imports: [DepositionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
