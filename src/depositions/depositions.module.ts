73a2e22a9386bbc4d47e333e20b4cc3bd9915065
Ederson Rodrigues <ederson@trilhante.com.br>
2023-07-12 18:30:20 -0300

    :sparkles: create depositions home controller

--- src/depositions/depositions.module.ts
+++ src/depositions/depositions.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DepositionsService } from './depositions.service';
 import { DepositionsController } from './depositions.controller';
+import { Deposition } from './entities/deposition.entity';
+import { DepositionHomeController } from './deposition-home.controller';
 
 @Module({
-  controllers: [DepositionsController],
+  imports: [TypeOrmModule.forFeature([Deposition])],
+  controllers: [DepositionsController, DepositionHomeController],
   providers: [DepositionsService]
 })
-export class DepositionsModule {}
+export class DepositionsModule { }
